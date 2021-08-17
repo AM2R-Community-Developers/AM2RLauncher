11f496c04fa025653dd03ff744b868f791dc10f7
Miepee <38186597+Miepee@users.noreply.github.com>
2021-08-17 10:43:07 +0200

    Update version number for release

--- AM2RLauncher/AM2RLauncher/LauncherUpdater.cs
+++ AM2RLauncher/AM2RLauncher/LauncherUpdater.cs
@@ -20,7 +20,7 @@ namespace AM2RLauncher
     public class LauncherUpdater
     {
         /// <summary>The Version that identifies this current release.</summary>
-        static readonly public string VERSION = "2.1.0";
+        static readonly public string VERSION = "2.1.1";
 
         /// <summary>The current Running platform.</summary>
         static readonly private Platform currentPlatform = Platform.Instance;   //needs to be declared here as well, because I can't access the one from eto
