018c00c6cc8f51283314f132ab531e99d03005b9
Alex <metroid3d2@gmail.com>
2021-08-29 14:42:33 -0700

    Update Splash.cs

--- AM2RLauncher/AM2RLauncher/MainForm/Splash.cs
+++ AM2RLauncher/AM2RLauncher/MainForm/Splash.cs
@@ -37,7 +37,12 @@ namespace AM2RLauncher
             "Reject C++, return to ABSTRACTION",
             "C# is just C++++ with Windows support.",
             "Use of the Launcher has now been authorized!",
-            "Oh? So you're patching me?"
+            "Oh? So you're patching me?",
+            "Oi Josuke! I used ZA HANDO to erase the broken pipe!",
+            "I, Gawron Giovana, have a dream.",
+            "Oooh weee ooo, I look just like an Omega"
+        
+            
         };
 
         /// <summary>
@@ -57,7 +62,9 @@ namespace AM2RLauncher
             "Go compile your own girlfriend. This one doesn't count, she's out of your league.",
             "Year 3115, NVIDIA still doesn't support Wayland.",
             "Was a mouse really that expensive, i3 users!?",
-            "Imagine using non-free software."
+            "Imagine using non-free software.",
+            "What if you used a non FOSS OS? Haha jk jk... Unless?"
+            
         };
 
         /// <summary>
