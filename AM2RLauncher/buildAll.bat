cd26a1a94508a5601b63c2955f880f4f7a32932a
Noah Snelson <noah.snelson@protonmail.com>
2025-01-28 21:12:16 -0800

    Update Linux runtime identifier in buildAll.bat

--- AM2RLauncher/buildAll.bat
+++ AM2RLauncher/buildAll.bat
@@ -4,6 +4,5 @@ dotnet publish AM2RLauncher.Wpf -c release -r win-x86 -o "builds\win86"
 ROBOCOPY "builds\win86" "builds\win86\lib\ " /XF *.exe *.config *.manifest /XD lib logs data /E /IS /MOVE
 move .\builds\win86\AM2RLauncher.Wpf.exe .\builds\win86\AM2RLauncher.exe
 move .\builds\win86\AM2RLauncher.Wpf.exe.config .\builds\win86\AM2RLauncher.exe.config
-dotnet publish AM2RLauncher.Gtk -p:PublishSingleFile=true -p:DebugType=embedded -c release -r ubuntu.18.04-x64 --no-self-contained -o "builds\linux64"
-dotnet publish AM2RLauncher.Gtk -p:PublishSingleFile=true -p:DebugType=embedded -c release -r ubuntu.18.04-x64 --self-contained -o "builds\linux64-selfContained"
-
+dotnet publish AM2RLauncher.Gtk -p:PublishSingleFile=true -p:DebugType=embedded -c release -r linux-x64 --no-self-contained -o "builds\linux64"
+dotnet publish AM2RLauncher.Gtk -p:PublishSingleFile=true -p:DebugType=embedded -c release -r linux-x64 --self-contained -o "builds\linux64-selfContained"
